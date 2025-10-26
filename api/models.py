fea677701ced1dead7b289ebcdde16509cee77ae
Justin Knuth <req.kyoshi@gmail.com>
2025-10-26 14:16:13 -0400

    feat(api): defined models

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Todo(BaseModel):
+    id: int
+    task: str
+    completed: bool = False
+    
+class TodoCreate(BaseModel):
+    task: str
+    
+class TodoUpdate(BaseModel):
+    task: Optional[str] = None
+    completed: Optional[bool] = None
